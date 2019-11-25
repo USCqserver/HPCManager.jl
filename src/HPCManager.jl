93ea4bcd719ce61a201d67bb438c729fc5c24351
Huo Chen <neversakura@hotmail.com>
2019-11-24 21:37:04 -0800

    fix bug: close_workers() will not close workers with id 1

--- src/HPCManager.jl
+++ src/HPCManager.jl
@@ -4,30 +4,30 @@ using Reexport
 @reexport using Distributed
 export init_cluster, close_workers
 
-function init_cluster(method=:worker_per_task; kwargs...)
+function init_cluster(method = :worker_per_task; kwargs...)
     host_name = gethostname()
     node_list = get_node_list()
     tpn = get_task_per_node()
     tpn = [parse(Int, i) for i in tpn]
     if (length(node_list) == 1) && (method == :worker_per_node)
-		@info "Only one nodes exists, skip cluster initialization."
-		nothing
-	elseif (length(node_list) == 1) && (method == :worker_per_task)
+        @info "Only one nodes exists, skip cluster initialization."
+        nothing
+    elseif (length(node_list) == 1) && (method == :worker_per_task)
         @info "Task is assigned to a single node. Starting with local manager..."
-        addprocs(tpn[1]-1; kwargs...)
-	elseif (length(node_list) > 1) && (method == :worker_per_node)
-		machine = []
-		for i in zip(node_list, tpn)
+        addprocs(tpn[1] - 1; kwargs...)
+    elseif (length(node_list) > 1) && (method == :worker_per_node)
+        machine = []
+        for i in zip(node_list, tpn)
             if i[1] == host_name
-				num = i[2]>1 ? 1 : 0
+                num = i[2] > 1 ? 1 : 0
                 push!(machine, (i[1], num))
             else
                 push!(machine, (i[1], 1))
             end
         end
-		@info "Task is assigned to multiple nodes. Starting with SSH manager..."
-		@info "Machine configuration: " machine
-		addprocs(machine; kwargs...)
+        @info "Task is assigned to multiple nodes. Starting with SSH manager..."
+        @info "Machine configuration: " machine
+        addprocs(machine; kwargs...)
     elseif (length(node_list) > 1) && (method == :worker_per_task)
         if length(tpn) == 1
             tpn = fill(tpn[1], length(node_list))
@@ -35,7 +35,7 @@ function init_cluster(method=:worker_per_task; kwargs...)
         machine = []
         for i in zip(node_list, tpn)
             if i[1] == host_name
-                push!(machine, (i[1], i[2]-1))
+                push!(machine, (i[1], i[2] - 1))
             else
                 push!(machine, i)
             end
@@ -47,10 +47,12 @@ function init_cluster(method=:worker_per_task; kwargs...)
 end
 
 function close_workers()
-	@info "Closing all workers..."
+    @info "Closing all workers..."
     for i in workers()
-		rmprocs(i)
-	end
+        if i != 1
+            rmprocs(i)
+        end
+    end
 end
 
 function get_node_list()
@@ -60,7 +62,7 @@ function get_node_list()
     if re === nothing
         return [slurm_node]
     else
-        raw_str = strip(re.match, ['[',']'])
+        raw_str = strip(re.match, ['[', ']'])
         raw_str = split(raw_str, ',')
         res = []
         for str_iter in raw_str
@@ -68,11 +70,11 @@ function get_node_list()
                 sp = split(str_iter, '-')
                 st = parse(Int, sp[1])
                 en = parse(Int, sp[2])
-                for n in st:en
-                    push!(res, "hpc"*lpad(n,4,'0'))
+                for n = st:en
+                    push!(res, "hpc" * lpad(n, 4, '0'))
                 end
             else
-                push!(res, "hpc"*str_iter)
+                push!(res, "hpc" * str_iter)
             end
         end
         return res
@@ -83,13 +85,13 @@ function get_task_per_node()
     tpn = ENV["SLURM_TASKS_PER_NODE"]
     @debug "Slurm tasks per node: " tpn
     res = []
-    raw_str = split(tpn,',')
+    raw_str = split(tpn, ',')
     for str_iter in raw_str
         if occursin("x", str_iter)
             x_idx = findfirst(isequal('x'), str_iter)
             str = str_iter[1:x_idx-2]
             num = parse(Int, str_iter[x_idx+1:end-1])
-            for i in 1:num
+            for i = 1:num
                 push!(res, str)
             end
         else
